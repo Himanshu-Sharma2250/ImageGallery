4936659a83a5b0e05e961d82be7f085236b3cc55
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-10-31 15:27:28 +0530

    add addImagesInAlbum controller

--- backend/src/controllers/album.controller.js
+++ backend/src/controllers/album.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import { Album } from "../models/album.model";
+import { Image } from "../models/image.model";
 
 // 1. create the album
 export const createAlbum = async (req, res) => {
@@ -65,7 +67,7 @@ export const getAlbum = async (req, res) => {
     }
 
     try {
-        const album = await Album.findById(album_id);
+        const album = await Album.findById(album_id).populate("images");
 
         if (!album) {
             return res.status(400).json({
@@ -89,3 +91,65 @@ export const getAlbum = async (req, res) => {
 };
 
 // 3. store the images in the album
+export const addImagesInAlbum = async (req, res) => {
+    const {albumId} = req.params;
+    const {imageIds} = req.body;
+
+    if (!imageIds | !albumId) {
+        return res.status(400).json({
+            message: "ImageId or AlbumId not found"
+        })
+    }
+
+    try {
+        const ids = imageIds.map(id => mongoose.Types.ObjectId(id)); // convert string in object
+        console.log("The ids in body: ", ids);
+
+        const isImagesExist = await Image.find({
+            _id: {$in: ids}
+        }).select('_id');
+
+        if (isImagesExist.length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Images doesnot exist"
+            })
+        }
+
+        const isAlbumExists = await Album.findById(albumId);
+
+        if (!isAlbumExists) {
+            return res.status(400).json({
+                success: false,
+                message: "Album doesnot exist"
+            })
+        }
+
+        const addingImages = await Album.findByIdAndUpdate(albumId, {
+            $addToSet: {
+                images: {
+                    $each: ids
+                }
+            }
+        }, {new: true})
+
+        if (addingImages.length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Insertion of Images failed"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Addition of Images in Album completed",
+            album_data: addingImages
+        })
+    } catch (error) {
+        console.error("Error adding images album: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Error adding images in album"
+        })
+    }
+}; 
--- backend/src/models/album.model.js
+++ backend/src/models/album.model.js
@@ -8,7 +8,11 @@ const albumSchema = new Schema({
     },
     description: {
         type: String
-    }
+    },
+    images: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Image' 
+    }]
 }, {timestamps: true});
 
 export const Album = mongoose.model("Album", albumSchema);
