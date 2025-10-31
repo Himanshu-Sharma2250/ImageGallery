3d048e4f6f3555904d9b2ff43e28f854dc46c1eb
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-10-31 15:48:18 +0530

    added middleware in upload route

--- backend/src/routes/image.route.js
+++ backend/src/routes/image.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getImage, uploadImage } from "../controllers/image.controller";
+import { upload } from "../middlewares/multer.middleware";
 
 const imageRouter = express.Router();
 
-imageRouter.post("/upload", uploadImage);
+imageRouter.post("/upload", upload, uploadImage);
 imageRouter.get("/:id", getImage);
 
 export default imageRouter;
